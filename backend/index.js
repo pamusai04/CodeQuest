64b8c7298f8b5beade24d75dc72ca67d3904f391
pamusai <pamusai04102003@gmail.com>
2026-08-29 16:55:35 +0530

    render deployment setup

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const submitRouter = require('./src/routes/submit');
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://codequest-rkjo.onrender.com', 
     credentials: true
 }));
 
